98ce704ee9203f1d4172610f7b3ed1d1cc769066
wsunetlab <sensors@ecs322-4.(none)>
2013-06-11 17:49:23 -0700

    saving before removing comments

--- daemon/topologyConfig.pl
+++ daemon/topologyConfig.pl
@@ -4,30 +4,33 @@
 # 0.0 <= p <= 1.0
 
 #use strict;
-use DateTime;
-use POSIX qw(strftime);
+  use DateTime;
+  use POSIX qw(strftime);
 
-my $_PMAX = 5; # TODO: Right now set to max - change if needed; min val:1 and max val:31
-my $_PMIN = 1;
-my $_PDELTA = 1;
+  my $_PMAX = 5; # TODO: Right now set to max - change if needed; min val:1 and max val:31
+  my $_PMIN = 1;
+  my $_PDELTA = 1;
 
-my $_DMAX = 90;
-my $_DTHETA = 20;
+  my $_DMAX = 90;
+  my $_DTHETA = 20;
 
-my $node = $ARGV[0];
-my $stepperSerial = $ARGV[1];
-my $userNodeList = $ARGV[2];
+#Arguments Start
+  my $node = $ARGV[0];
+  my $stepperSerial = $ARGV[1];
+  my $userNodeList = $ARGV[2];
 
-my $jobID = $ARGV[3];
-my $topologyId = $ARGV[4];
+  my $jobID = $ARGV[3];
+  my $topologyId = $ARGV[4];
+#Arguments End
 
-my @Lu;
 
-if(@ARGV == 0 || @ARGV <=4){
-  print "No Arguments found\n";
-  print "usage: perl topologyConfig.pl nodeId stepperserial userNodelist jobId topologyId\n";
-  exit(0);
-}
+  my @Lu;
+
+  if(@ARGV == 0 || @ARGV <=4){
+    print "No Arguments found\n";
+    print "usage: perl topologyConfig.pl nodeId stepperserial userNodelist jobId topologyId\n";
+    exit(0);
+  }
 
 #my $Lu;
 
@@ -35,31 +38,30 @@ if(@ARGV == 0 || @ARGV <=4){
 #print strftime("%a, %d %b %Y %H:%M:%S %z", localtime(time())) . "\n";
 #print "\n";
 
-if (defined($userNodeList)) {
-  @Lu = split(',', $userNodeList);
- # print "Lu Size:", scalar @Lu ,"\n";
-} else {
-  @Lu = (); # user wants this node to connect to no other nodes
-}
+  if (defined($userNodeList)) {
+    @Lu = split(',', $userNodeList);
+   # print "Lu Size:", scalar @Lu ,"\n";
+  } else {
+    @Lu = (); # user wants this node to connect to no other nodes
+  }
 
-foreach my $val (@Lu) {
+  foreach my $val (@Lu) {
    # print "$val\n";
   }
 
-my $p = $_PMAX;
-my @Lr;
-my $bestError = $#Lu;
-my $d = 0;
-#just for testing
-$d =20;
-my $bestD = $d;
-my $bestP = $p;
+  my $p = $_PMAX;
+  my @Lr;
+  my $bestError = $#Lu;
+  my $d = 0;
+  #just for testing
+  $d =20;
+  my $bestD = $d;
+  my $bestP = $p;
 
-chdir("/var/www/web/daemon") or die "$1";
-# to get current directory
+  chdir("/var/www/web/daemon") or die "$1";
 
 
-my $_ROTATE = "/usr/lib/jvm/java-6-openjdk/bin/java -cp .:/var/www/web/daemon/phidget21.jar Rotate $stepperSerial";
+  my $_ROTATE = "/usr/lib/jvm/java-6-openjdk/bin/java -cp .:/var/www/web/daemon/phidget21.jar Rotate $stepperSerial";
 
 # May 23 2013: phidget21.jar has been added to classpath, so no need to pass explicitly. 
 #my $_ROTATE = "/usr/lib/jvm/java-6-openjdk/bin/java Rotate $stepperSerial";
@@ -72,32 +74,37 @@ my $_ROTATE = "/usr/lib/jvm/java-6-openjdk/bin/java -cp .:/var/www/web/daemon/ph
 # May 23 2013: Mysql.jar has been added to classpath, so no need to pass explicitly. 
 #my $_HELLO = "/usr/lib/jvm/java-6-openjdk/bin/java hellomote.LoadPrograms";
 #my $_HELLO = "perl LoadPrograms.pl";
-my $_HELLO = "/bin/bash envsetup.sh $jobID $topologyId";  
 
-#while (1) {
-$i = 0;
-while($i <= 1){
-#  print "$_ROTATE $d";
-#  print "Applied Degree : $d\n";
-chdir("/var/www/web/daemon") or die "$1";
-	my $out1 = system("$_ROTATE $d");
-	
-sleep(10);
+  my $_HELLO = "/bin/bash envsetup.sh $jobID $topologyId";  
+
+  #while (1) { #original loop
+  $i = 0;
+  while($i <= 1){
+  #  print "$_ROTATE $d";
+  #  print "Applied Degree : $d\n";
+  chdir("/var/www/web/daemon") or die "$1";
+  my $out1 = system("$_ROTATE $d");
+  # wait for rotation to finish	
+  sleep(5);
+
 #  my $out = system("$_ROTATE");
 # print "Java output:$out1\n";
 #  print `$_ROTATE $d\n`;
 #  print "done rotating\n";
+
   @Lr = ();
   my $currentError = 0;
-  # TODO: send hello message and collect acks;
+  
+# TODO: send hello message and collect acks;
   #  while (0) {
 # Run Hello world ack code 
 #my $ackList = `$_HELLO $p`;
 
-chdir("/var/www/web/daemon/hellomote") or die "$1";
-my $ackList = `$_HELLO $p`;
+  chdir("/var/www/web/daemon/hellomote") or die "$1";
+#TODO: make changes here for acknowledgment list
+  my $ackList = `$_HELLO $p`;
 
-sleep(20);
+  sleep(20);
 #print "Ack List Output: $ackList\n";
 #my $ackList = `$_HELLO $p`;  
 #  sleep(80);
@@ -113,13 +120,14 @@ sleep(20);
         $currentError++; # increment error
       }
     }
+
     if ($currentError < $bestError ||
         ($currentError == $bestError && $p < $bestP)) {
       $bestError = $currentError;
       $bestD = $d;
       $bestP = $p;
     }
-  }
+  }  #for each ack loop ends
 
 
   if ($bestError == 0 || $d == $_DMAX) {
@@ -139,19 +147,20 @@ sleep(20);
  # print "New Degree d: $d \n";
  # sleep(0);
 
-$i++;
-}
+  $i++;
+  } #while(1) loop ends here
 
 # results:
 #print "Best Degree D :$bestD\n";
 #print "Best Tranmission Power P: $bestP\n";
 #print "New Node List: @Lr\n";
 
-open (TOPDATA, ">/var/www/web/daemon/hellomote/Topology_Result.summary");
-print TOPDATA "$bestD\n";
-print TOPDATA "$bestP\n";
-print TOPDATA "@Lr\n";
-close TOPDATA;
+  open (TOPDATA, ">/var/www/web/daemon/hellomote/Topology_Result.summary");
+  print TOPDATA "$bestD\n";
+  print TOPDATA "$bestP\n";
+  print TOPDATA "@Lr\n";
+  close TOPDATA;
+
 #print "End time:";
 #print DateTime->now()->strftime("%a, %d %b %Y %H:%M:%S %z");
 #print strftime("%a, %d %b %Y %H:%M:%S %z", localtime(time())) . "\n";
